8b84b5d30ea22967c2a9fd4b4261ea30b38819b9
Narain Singaram <narain.awesome@gmail.com>
2023-01-03 00:34:17 -0500

    changing text area

--- main.py
+++ main.py
@@ -72,6 +72,8 @@ def get_sentiment_description(text):
 
 
 # Streamlit User Interface
+st.beta_set_page_config(page_title='Sentiment Finder', page_icon = "😀")
+
 st.header("Python Sentiment Finder")
 txt_box_ipt = st.text_area('Enter a statement below to find its overall sentiment/emotion', height=300)
 submit = st.button('Generate')
